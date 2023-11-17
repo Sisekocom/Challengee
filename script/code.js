29a128e4aee0e2b92e6a176e00b4caae5ee40a38
Sisekocom <nonyongos1@gmail.com>
2023-11-17 16:33:26 +0200

    update

---
+++ script/code.js
@@ -0,0 +1,14 @@
+let heading = document.getElementById('heading')
+heading.textContent='Java script'
+
+let paragraph = document.querySelector('.paragraph')
+paragraph.textContent='This is a language I have mastered and I am proud of myself. I complished this because I believed in myself'
+
+let headiing = document.getElementsByTagName('h3')[0]
+headiing.textContent='have accomplished so far'
+
+
+let pa = document.getElementsByTagName('p')[1]
+pa.textContent='I have discovered that I can accomplish anything I put my mind to'
+
+let input =document.querySelector('Multiply')
